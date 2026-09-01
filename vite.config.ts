69ce694cf887d0a7d8339323a0e9e14758237c5f
eduardbalan325 <eduardbalan325@gmail.com>
2026-09-01 14:48:12 -0700

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/portfolio/",
+  vite: {
+    base: "/portfolio/",
+  },
+
   tanstackStart: {
-    server: { entry: "server" },
+    server: {
+      entry: "server",
+    },
 
     spa: {
+      enabled: true,
       prerender: {
-        outputPath: "/index.html",
         crawlLinks: true,
       },
     },
